b4c59855547fd8a6ce2cd635fc7804563433ac65
artofnext <novnovalex@gmail.com>
2019-11-12 16:12:04 +0200

    Modal window

---
+++ js/modal_button.js
@@ -0,0 +1,30 @@
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("showModal");
+let span = document.getElementsByClassName("close")[0];
+let counterClose = 0;
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+span.onmouseover = function() {
+    this.style.float=(this.style.float == 'left') ? 'right' : 'left';
+    counterClose ++;
+    if (counterClose > 2) {
+        let modalContent = document.getElementsByClassName('modal-content')[0];
+        modalContent.style = 'visibility: visible;'
+    }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// let modalContent = document.getElementsByClassName('modal-content')[0];
+// modalContent.style = 'background-image:url(img/boromir.png);'
