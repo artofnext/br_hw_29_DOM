e0ab9afea78c19b01109332086fe4ad82a886b2a
artofnext <novnovalex@gmail.com>
2019-11-12 17:37:07 +0200

    traffic light ready

--- js/traffic_light.js
+++ js/traffic_light.js
@@ -1,4 +1,23 @@
-let counter = 0;
-function switchLight() {
+let counter = 1;
 
+function switchLight() {
+    document.getElementById('red').classList.remove('active');
+    document.getElementById('yellow').classList.remove('active');
+    document.getElementById('green').classList.remove('active');
+
+    switch(counter % 4) {
+    case 0:
+        document.getElementById('red').classList.add('active');
+        break;
+    case 1:
+        document.getElementById('yellow').classList.add('active');
+        break;
+    case 2:
+        document.getElementById('green').classList.add('active');
+        break;
+    case 3:
+        document.getElementById('yellow').classList.add('active');
+        break;
+    }
+    counter ++;
 }
